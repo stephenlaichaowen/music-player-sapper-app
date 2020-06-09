220cae01755fb121a8aab95caf48aa5fcd53d571
stephenlai <stephenlai2015@gmail.com>
2020-06-09 23:43:24 +0800

    add pwa2apk feature

--- src/routes/db/songs.js
+++ src/routes/db/songs.js
@@ -1,84 +1,75 @@
 const songs = [
   {
     id: 0,
-    name: "Mekanın Sahibi",
-    artist: "Norm Ender",
-    cover: "img/1.jpg",
-    source: "mp3/1.mp3",
-    url: "https://www.youtube.com/watch?v=z3wAjJXbYzA",
+    name: "光年之外 (LIGHT YEARS AWAY)",
+    artist: "鄧紫琪 G.E.M.",
+    cover: "https://stephenlaichaowen.github.io/my-assets/music-player/img/1.jpg",
+    source: "https://stephenlaichaowen.github.io/my-assets/music-player/mp3/1.mp3",
+    url: "https://www.youtube.com/watch?v=T4SimnaiktU&list=PLA9x9-eADvOoGjq1ce-FU0gpdMuq-Rk1l",
     favorited: false
   },
   {
     id: 1,
-    name: "Everybody Knows",
-    artist: "Leonard Cohen",
-    cover: "img/2.jpg",
-    source: "mp3/2.mp3",
-    url: "https://www.youtube.com/watch?v=Lin-a2lTelg",
+    name: "那些年，我們一起追的女孩",
+    artist: "胡夏",
+    cover: "https://stephenlaichaowen.github.io/my-assets/music-player/img/2.jpg",
+    source: "https://stephenlaichaowen.github.io/my-assets/music-player/mp3/2.mp3",
+    url: "https://www.youtube.com/watch?v=xWzlwGVQ6_Q&list=PLA9x9-eADvOoGjq1ce-FU0gpdMuq-Rk1l&index=4",
     favorited: true
   },
   {
     id: 2,
-    name: "Extreme Ways",
-    artist: "Moby",
-    cover: "img/3.jpg",
-    source: "mp3/3.mp3",
-    url: "https://www.youtube.com/watch?v=ICjyAe9S54c",
+    name: "告白氣球 Love Confession",
+    artist: "周杰倫 Jay Chou",
+    cover: "https://stephenlaichaowen.github.io/my-assets/music-player/img/3.jpg",
+    source: "https://stephenlaichaowen.github.io/my-assets/music-player/mp3/3.mp3",
+    url: "https://www.youtube.com/watch?v=bu7nU9Mhpyo&list=PLA9x9-eADvOoGjq1ce-FU0gpdMuq-Rk1l&index=2",
     favorited: false
   },
   {
     id: 3,
-    name: "Butterflies",
-    artist: "Sia",
-    cover: "img/4.jpg",
-    source: "mp3/4.mp3",
-    url: "https://www.youtube.com/watch?v=kYgGwWYOd9Y",
+    name: "是我不夠好 (Not Good Enough)",
+    artist: "李毓芬 Tia Lee",
+    cover: "https://stephenlaichaowen.github.io/my-assets/music-player/img/4.jpg",
+    source: "https://stephenlaichaowen.github.io/my-assets/music-player/mp3/4.mp3",
+    url: "https://www.youtube.com/watch?v=BsvIwqyiaJw&list=PLA9x9-eADvOoGjq1ce-FU0gpdMuq-Rk1l&index=78",
     favorited: false
   },
   {
     id: 4,
-    name: "The Final Victory",
-    artist: "Haggard",
-    cover: "img/5.jpg",
-    source: "mp3/5.mp3",
-    url: "https://www.youtube.com/watch?v=0WlpALnQdN8",
+    name: "我的少女時代 (Our Times)",
+    artist: "田馥甄 Hebe Tien",
+    cover: "https://stephenlaichaowen.github.io/my-assets/music-player/img/5.jpg",
+    source: "https://stephenlaichaowen.github.io/my-assets/music-player/mp3/5.mp3",
+    url: "https://www.youtube.com/watch?v=_sQSXwdtxlY&list=PLA9x9-eADvOoGjq1ce-FU0gpdMuq-Rk1l&index=3",
     favorited: true
   },
   {
     id: 5,
-    name: "Genius ft. Sia, Diplo, Labrinth",
-    artist: "LSD",
-    cover: "img/6.jpg",
-    source: "mp3/6.mp3",
-    url: "https://www.youtube.com/watch?v=HhoATZ1Imtw",
+    name: "手掌心 (Palm)",
+    artist: "丁噹 Della",
+    cover: "https://stephenlaichaowen.github.io/my-assets/music-player/img/6.jpg",
+    source: "https://stephenlaichaowen.github.io/my-assets/music-player/mp3/6.mp3",
+    url: "https://www.youtube.com/watch?v=7wvNwOPprBE&list=PLA9x9-eADvOoGjq1ce-FU0gpdMuq-Rk1l&index=21",
     favorited: false
   },
-  {
+	{
     id: 6,
-    name: "The Comeback Kid",
-    artist: "Lindi Ortega",
-    cover: "img/7.jpg",
-    source: "mp3/7.mp3",
-    url: "https://www.youtube.com/watch?v=me6aoX0wCV8",
-    favorited: true
+   	name: "我的歌聲裡 (You Exist In My Song)",
+    artist: "曲婉婷 Wanting",
+    cover: "https://stephenlaichaowen.github.io/my-assets/music-player/img/7.jpg",
+    source: "https://stephenlaichaowen.github.io/my-assets/music-player/mp3/7.mp3",
+    url: "https://www.youtube.com/watch?v=w0dMz8RBG7g&list=PLA9x9-eADvOoGjq1ce-FU0gpdMuq-Rk1l&index=9",
+    favorited: false
   },
   {
     id: 7,
-    name: "Overdose",
-    artist: "Grandson",
-    cover: "img/8.jpg",
-    source: "mp3/8.mp3",
-    url: "https://www.youtube.com/watch?v=00-Rl3Jlx-o",
-    favorited: false
-  },
-  {
-    id: 8,
-    name: "Rag'n'Bone Man",
-    artist: "Human",
-    cover: "img/9.jpg",
-    source: "mp3/9.mp3",
+   	name: "變心的翅膀",
+    artist: "陳明真",
+    cover: "https://stephenlaichaowen.github.io/my-assets/music-player/img/8.jpg",
+    source: "https://stephenlaichaowen.github.io/my-assets/music-player/mp3/8.mp3",
     url: "https://www.youtube.com/watch?v=L3wKzyIN1yk",
-    favorited: false
+    favorited: true
   }
 ]
 
